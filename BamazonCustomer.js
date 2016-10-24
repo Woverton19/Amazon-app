7d15159b817ba749d82ced6afae9bba2041d4718
John Overton <John Overton>
2016-10-24 18:23:34 -0500

    second push have the mysql loading and showing in node working with inquirer

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,13 +13,50 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId);
 })
 
-connection.query('SELECT * FROM products', function(err,rows){
-	// console.log(rows.products);
-	var choiceArray = [];
-                for (var i = 0; i < rows.length; i++) {
-                    choiceArray.push(rows[i].ProductName);
+connection.query('SELECT * FROM products', function(err,res){
+	// console.log(res);
+	
+                for (var i = 0; i < res.length; i++) {
+                    console.log("\nProduct ID:" + res[i].Itemid + "\nProduct Name:" + res[i].ProductName + "\nDepartment Name:" + res[i].DepartmentName + "\nPrice:" + res[i].Price + "\nStock Quantity:" + res[i].StockQuantity);
                 }
-                console.log(choiceArray)
+               
 });
+// connection.end();
+inquirer.prompt([{
+	name: "idnumber",
+        type: "input",
+        message: "What Product would you like to buy? ",
+        validate: function(value) {
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    // }, {
+    //     name: "end",
+    //     type: "input",
+    //     message: "How many would you like to order?",
+    //     validate: function(value) {
+    //         if (isNaN(value) == false) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+
+}]).then(function(answer) {
+        var query = 'SELECT ProductName,DepartmentName,Price,StockQuantity FROM Products WHERE Itemid ?';
+        connection.query(query, [answer.idnumber], function(err, res) {
+            for (var i = 0; i < res.length; i++) {
+            	console.log("\n****************************************************************************************");
+                console.log("\nProduct ID:" + res[i].Itemid + "\nProduct Name:" + res[i].ProductName + "\nDepartment Name:" + res[i].DepartmentName + "\nPrice:" + res[i].Price + "\nStock Quantity:" + res[i].StockQuantity);
+            	console.log("\n****************************************************************************************");
+            }
+        })
+        connection.end();
+    });
+
+
 
 connection.end();
